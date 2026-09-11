1256c225b893c595846926a1856d0dc5e5d908cf
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2026-09-10 21:08:00 -0500

    se importa fast api se asigna un mensaje inicial

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
-import json
+'''import json
 with open("../data/clientes.json", "r", encoding="utf-8") as file: #encoding evita problemas si existen tildes o caracteres especiales dentro de nombres o el archivo
     clientes = json.load(file)
 
     for cliente in clientes: #recorremos la lista de diccionarios
         print(cliente["nombre"])
 
+'''
+
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+
+def inicio():
+    return {"Mensaje: Conexion API completada"}
+#ejecute la api con el uvicorn
+#uvicorn app.main:app --reload --host 127.0.0.1
